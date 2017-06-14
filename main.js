d9a7eda58c084d8e8bd610b9d0bc27c96059e3ef
Zach Morse <zacharymorse@gmail.com>
2017-06-14 14:37:21 -0500

    minimal functionality API call

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 // 1. Fetch your users data
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
-(function () {
 
-  'use strict';
+var dataDisplay = document.querySelector(".customers");
 
-  // Your Code Goes Here
+function customerImport() {
+  "use strict";
+  fetch("https://randomuser.me/api/?results=12") // this is a PROMISE, a placeholder for future data
+    .then(function(dataFromAPI) {
+      return dataFromAPI.json();
+    })
+    .then(function(data) {
+      var people = data.results;
+      dataDisplay.innerHTML = people[0].name;
+      console.log(data.results.gender);
+    });
+}
 
-})();
+customerImport();
+
+dataDisplay.innerHTML = "WAITING";
+
+// (function() {
+//   "use strict";
+//   fetch("https://randomuser.me/api/?results=12");
+//   // Your Code Goes Here
+// })();
