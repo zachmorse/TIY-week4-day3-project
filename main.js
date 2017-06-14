cbc2f0001773117b21e490866241fdf033aad13a
Zach Morse <zacharymorse@gmail.com>
2017-06-14 17:03:51 -0500

    JS mostly finished, working on CSS

--- main.js
+++ main.js
@@ -12,17 +12,55 @@ function customerImport() {
     })
     .then(function(data) {
       var people = data.results;
-      dataDisplay.innerHTML = people[0].name;
-      console.log(data.results.gender);
+      for (var i = 0; i < people.length; i++) {
+        makeProfile(people[i]);
+      }
+      console.log(data.results);
     });
 }
 
-customerImport();
+customerImport(); // function defined above called here
 
-dataDisplay.innerHTML = "WAITING";
+function makeProfile(data) {
+  function createWrapper() {
+    var profileBox = document.createElement("div");
+    profileBox.classList.add("profileBox");
+    dataDisplay.appendChild(profileBox);
 
-// (function() {
-//   "use strict";
-//   fetch("https://randomuser.me/api/?results=12");
-//   // Your Code Goes Here
-// })();
+    var pic = document.createElement("img");
+    pic.src = data.picture.large;
+    pic.classList.add("pic");
+    profileBox.appendChild(pic);
+
+    var fullName = document.createElement("h2");
+    fullName.classList.add("fullName");
+    fullName.textContent = data.name.first + " " + data.name.last;
+    profileBox.appendChild(fullName);
+
+    var email = document.createElement("h4");
+    email.classList.add("email");
+    email.textContent = data.email;
+    profileBox.appendChild(email);
+
+    var streetAddress = document.createElement("p");
+    streetAddress.classList.add("streetAddress");
+    streetAddress.textContent = data.location.street;
+    profileBox.appendChild(streetAddress);
+
+    var cityAndState = document.createElement("p");
+    cityAndState.classList.add("cityAndState");
+    cityAndState.textContent =
+      data.location.city +
+      ", " +
+      data.location.state +
+      " " +
+      data.location.postcode;
+    profileBox.appendChild(cityAndState);
+
+    var phoneNum = document.createElement("p");
+    phoneNum.classList.add("phoneNum");
+    phoneNum.textContent = data.phone;
+    profileBox.appendChild(phoneNum);
+  }
+  createWrapper();
+}
